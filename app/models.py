ece35dcaa610c1554dded0be6583ea355e03d4e7
ZeFelix <jophev3@hotmail.com>
2018-09-19 22:21:12 -0300

    criado restando dos campos do aluno

--- app/models.py
+++ app/models.py
@@ -6,17 +6,39 @@ from rest_framework.filters import OrderingFilter
 
 class Classes(models.Model):
     name = models.CharField("Nome da Turma", max_length=50)
-    course = models.ForeignKey("Course", verbose_name="Curso", on_delete=models.CASCADE,blank=True, null=True)
+    course = models.ForeignKey(
+        "Course", verbose_name="Curso", on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
         return self.name
+
 
 class Student(models.Model):
+    SEX_CHOICES = (
+        ('M', 'Masculino'),
+        ('F', 'Feminio'),
+    )
+    CIVIL_STATUS_CHOICES = (
+        ('C', 'Casado'),
+        ('S', 'Solteiro'),
+    )
+    SCHOLL_ORIGIN_CHOICES = (
+        ('PB', 'Pública'),
+        ('PA', 'Particular'),
+    )
+
     name = models.CharField("Nome do Aluno", max_length=50)
-    classe = models.ForeignKey(Classes, verbose_name="Turma", on_delete=models.CASCADE)
+    sex = models.CharField("Sexo", max_length=2, choices=SEX_CHOICES, default="M")
+    color_blind = models.BooleanField("Daltônico", default=False)
+    ager = models.IntegerField("Idade",default=0)
+    civil_status = models.CharField("Estado civil", max_length=2, choices=CIVIL_STATUS_CHOICES,default="S")
+    school_origin = models.CharField("Origem Escolar", max_length=2, choices=SCHOLL_ORIGIN_CHOICES, default="PB")
+    classe = models.ForeignKey(
+        Classes, verbose_name="Turma", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
+
 
 class Activity(models.Model):
     name = models.CharField("Atividade", max_length=50)
@@ -24,25 +46,33 @@ class Activity(models.Model):
     def __str__(self):
         return self.name
 
+
 class Node(models.Model):
     name = models.CharField("Nome do Nós", max_length=50)
     activity = models.ManyToManyField(Activity, verbose_name="Atividades")
     students = models.ManyToManyField(Student, through="StudentInformations")
-    node_parent = models.ForeignKey("self", verbose_name="Nó Pai", on_delete=models.CASCADE, blank=True, null=True)
-    course = models.ForeignKey("Course", verbose_name="Curso que o nó pertence", on_delete=models.CASCADE, blank=True, null=True)
-    node_end = models.BooleanField("Nó fim (Folha)?",default=False)
-    node_start = models.BooleanField("Nó inicio",default=False)
+    node_parent = models.ForeignKey(
+        "self", verbose_name="Nó Pai", on_delete=models.CASCADE, blank=True, null=True)
+    course = models.ForeignKey("Course", verbose_name="Curso que o nó pertence",
+                               on_delete=models.CASCADE, blank=True, null=True)
+    node_end = models.BooleanField("Nó fim (Folha)?", default=False)
+    node_start = models.BooleanField("Nó inicio", default=False)
 
     def __str__(self):
-        return str(self.name) +"-"+str(self.get_activities())
-    
+        return str(self.name) + "-"+str(self.get_activities())
+
     def get_activities(self):
         return "\n".join([p.name for p in self.activity.all()])
-    
+
+
 class StudentInformations(models.Model):
-    node = models.ForeignKey(Node, verbose_name="Nome do Nós", on_delete=models.CASCADE)
-    student = models.ForeignKey(Student, verbose_name="Estudantes", on_delete=models.CASCADE)
-    notes = models.DecimalField("Nota do Aluno", max_digits=4, decimal_places=2,blank=True, null=True)
+    node = models.ForeignKey(
+        Node, verbose_name="Nome do Nós", on_delete=models.CASCADE)
+    student = models.ForeignKey(
+        Student, verbose_name="Estudantes", on_delete=models.CASCADE)
+    notes = models.DecimalField(
+        "Nota do Aluno", max_digits=4, decimal_places=2, blank=True, null=True)
+
 
 class Course(models.Model):
     name = models.CharField("Nome do Curso", max_length=50)
