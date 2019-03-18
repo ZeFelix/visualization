02b325c8c70efaa91e66f3ce29b6d6d04a230364
José Félix do Nascimento Neto <jophev3@hotmail.com>
2019-03-18 19:34:42 -0300

    Update settings.py

--- projetoTCC/settings.py
+++ projetoTCC/settings.py
@@ -115,16 +115,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'tcc',
-#         'USER': 'postgres',
-#         'PASSWORD': 'joseph',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
-#     }
-# }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
